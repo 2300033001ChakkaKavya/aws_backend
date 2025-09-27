4732144a2c2d1a591ddf835679b1b20c83ce367c
2300033001ChakkaKavya <2300033001@kluniversity.in>
2025-09-27 10:13:48 +0530

    Update CorsConfig.java

--- src/main/java/art/config/CorsConfig.java
+++ src/main/java/art/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://ec2-54-163-37-209.compute-1.amazonaws.com:8081")
+                        .allowedOrigins("http://ec2-44-220-158-40.compute-1.amazonaws.com:8081")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
